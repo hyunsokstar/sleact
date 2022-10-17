aab5593a3e26dc59fa9f7096f9b0e3c68fcc785d
hyun <hyunsokstar@gmail.com>
2022-10-17 11:11:35 +0900

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,29 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from '../ormconfig';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, ChannelsModule, DmsModule, WorkspacesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    ChannelsModule,
+    DmsModule,
+    WorkspacesModule,
+    TypeOrmModule.forRoot(ormConfig),
+  ],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
-
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): any {
-    consumer.apply(LoggerMiddleware).forRoutes('*')
+    consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
